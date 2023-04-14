ba18edc7d7587d0e28a6d33a9eea4f5506c49f6a
Adam-pw <adampithewan@gmail.com>
2023-04-14 11:27:30 +0530

    Changes in implementation

--- src/components/Tablecomp.tsx
+++ src/components/Tablecomp.tsx
@@ -9,8 +9,94 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import "./Tablecomp.css";
-import { data, Q1, Q2, Q3, Q4 } from "./metadata";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+
+const data = [
+  {
+    month: "January",
+    indicators: [
+      { name: "revenue", value: 10000 },
+      { name: "expenses", value: 8000 },
+    ],
+  },
+  {
+    month: "February",
+    indicators: [
+      { name: "revenue", value: 12000 },
+      { name: "expenses", value: 10000 },
+    ],
+  },
+  {
+    month: "March",
+    indicators: [
+      { name: "revenue", value: 9000 },
+      { name: "expenses", value: 7000 },
+    ],
+  },
+  {
+    month: "April",
+    indicators: [
+      { name: "revenue", value: 11000 },
+      { name: "expenses", value: 9000 },
+    ],
+  },
+  {
+    month: "May",
+    indicators: [
+      { name: "revenue", value: 8000 },
+      { name: "expenses", value: 6000 },
+    ],
+  },
+  {
+    month: "June",
+    indicators: [
+      { name: "revenue", value: 14000 },
+      { name: "expenses", value: 12000 },
+    ],
+  },
+  {
+    month: "July",
+    indicators: [
+      { name: "revenue", value: 9000 },
+      { name: "expenses", value: 7000 },
+    ],
+  },
+  {
+    month: "August",
+    indicators: [
+      { name: "revenue", value: 11000 },
+      { name: "expenses", value: 9000 },
+    ],
+  },
+  {
+    month: "September",
+    indicators: [
+      { name: "revenue", value: 10000 },
+      { name: "expenses", value: 8000 },
+    ],
+  },
+  {
+    month: "Octomber",
+    indicators: [
+      { name: "revenue", value: 12000 },
+      { name: "expenses", value: 10000 },
+    ],
+  },
+  {
+    month: "November",
+    indicators: [
+      { name: "revenue", value: 9000 },
+      { name: "expenses", value: 7000 },
+    ],
+  },
+  {
+    month: "December",
+    indicators: [
+      { name: "revenue", value: 11000 },
+      { name: "expenses", value: 9000 },
+    ],
+  },
+];
 
 const Tablecomp = () => {
   const [click, setClick] = useState(false);
@@ -21,33 +107,35 @@ const Tablecomp = () => {
     setClick(!click);
     switch (value) {
       case "Q1":
-        setSubdata(Q1);
+        setSubdata([data[0], data[1], data[2]]);
         break;
       case "Q2":
-        setSubdata(Q2);
+        setSubdata([data[4], data[3], data[5]]);
         break;
       case "Q3":
-        setSubdata(Q3);
+        setSubdata([data[6], data[7], data[8]]);
         break;
       case "Q4":
-        setSubdata(Q4);
+        setSubdata([data[9], data[10], data[11]]);
         break;
       default:
         setSubdata([]);
     }
   };
+
+  const abc = ["Q1", "Q2", "Q3", "Q4"];
   return (
     <TableContainer className="main">
       <Table size="md" className="table">
         <Thead className="head">
           <Tr>
             <Th>Values</Th>
-            {data.map((value, index) => {
+            {abc.map((month, index) => {
               return (
                 <>
                   <Th
                     onClick={() => {
-                      handleClick(value.time);
+                      handleClick(month);
                     }}
                     key={index}
                   >
@@ -58,15 +146,15 @@ const Tablecomp = () => {
                         alignItems: "center",
                       }}
                     >
-                      {value.time}{" "}
-                      {click && id === value.time ? (
+                      {month}
+                      {click && id === month ? (
                         <AiOutlineArrowLeft />
                       ) : (
                         <AiOutlineArrowRight />
                       )}
                     </div>
                   </Th>
-                  {click && id === value.time && (
+                  {click && id === month && (
                     <>
                       {subdata?.map((value: any, index: any) => {
                         return (
@@ -92,11 +180,11 @@ const Tablecomp = () => {
         <Tbody>
           <Tr>
             <Td>Revenue</Td>
-            {data.map((value, index) => {
+            {abc.map((value, index) => {
               return (
                 <>
-                  <Td key={index}>{value.indicators[0].value}</Td>
-                  {click && id === value.time && (
+                  <Td>800</Td>
+                  {click && id === value && (
                     <>
                       {subdata?.map((value: any, index: any) => {
                         return (
@@ -115,11 +203,12 @@ const Tablecomp = () => {
           </Tr>
           <Tr>
             <Td>Expense</Td>
-            {data.map((value, index) => {
+            {abc.map((value, index) => {
               return (
                 <>
-                  <Td key={index}>{value.indicators[1].value}</Td>
-                  {click && id === value.time && (
+                  <Td>1200</Td>
+                  {/* <Td key={index}>{value.indicators[1].value}</Td> */}
+                  {click && id === value && (
                     <>
                       {subdata?.map((value: any, index: any) => {
                         return (
